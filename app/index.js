be63ce53a8c4289d4bd820715b91ff2c33b8adc6
Manav Misra <manavm@visionwebsoft.com>
2023-01-10 21:23:31 -0600

    feat: start the express server

--- app/index.js
+++ app/index.js
@@ -1,1 +1,16 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+import express from "express";
+import config from "./config.js";
+import userRouter from "./user/routes.js";
+
+const app = express();
+
+// Logical OR short-circuits if the first value is truthy
+const PORT = config.port || 3000;
+
+app.use(express.json());
+
+app.use("/api/users", userRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
