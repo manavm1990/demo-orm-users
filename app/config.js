5053de8bf5f4ec4d773043a3137788b9a920cab8
Manav Misra <manavm@visionwebsoft.com>
2023-01-10 20:59:59 -0600

    Add config

---
+++ app/config.js
@@ -0,0 +1,14 @@
+// This is the only place from which we will access environment variables.
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+  db: {
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+  port: process.env.PORT,
+  saltRounds: process.env.SALT_ROUNDS,
+};
