94eee3ac3784abec2a087e46eb3ad6e7a0f5e43c
Manav Misra <manavm@visionwebsoft.com>
2023-01-12 19:36:35 -0600

    feat: sanitize user

--- app/user/model.js
+++ app/user/model.js
@@ -1,5 +1,15 @@
+import bcrypt from "bcrypt";
 import { DataTypes } from "sequelize";
+import config from "../config.js";
 import sequelize from "../conn.js";
+
+async function sanitizeUser(user) {
+  user.username = user.username.toLowerCase();
+  user.email = user.email.toLowerCase();
+
+  const salt = await bcrypt.genSalt(config.saltRounds);
+  user.password = await bcrypt.hash(user.password, salt);
+}
 
 const User = sequelize.define(
   "User",
@@ -50,5 +60,9 @@ await User.sync().catch((err) => {
   console.error(`Error syncing User model/table: ${err.message}`);
   process.exit(1);
 });
+
+// Hash password before creating the user in the DB.
+User.beforeCreate(sanitizeUser);
+User.beforeUpdate(sanitizeUser);
 
 export default User;
